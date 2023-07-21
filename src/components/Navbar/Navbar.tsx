3a6c2c114c44f652e96e39a879d309da10f78b73
Maxwell <nanayawfixing@gmail.com>
2023-07-21 09:37:52 +0000

    setting up tailwind css

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+// import '../../styles/global.css'
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <header className="bg-indigo-900 h-12" >
+    {/* <header className="sticky top-0 z-[1] mx-auto  flex w-full max-w-7xl flex-wrap items-center justify-between border-b border-gray-100 bg-background p-[2em] font-sans font-bold uppercase text-text-primary backdrop-blur-[100px] dark:border-gray-800 dark:bg-d-background dark:text-d-text-primary"> */}
+
+      Navbar
+    </header>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//aspect-square sticky top-0 z-[1] mx-auto  flex w-full max-w-7xl flex-wrap items-center justify-between border-b border-gray-100 bg-background p-[2em] font-sans font-bold uppercase text-text-primary backdrop-blur-[100px] dark:border-gray-800 dark:bg-d-background dark:text-d-text-primary
