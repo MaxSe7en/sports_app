1728e41b32bf7ce78f44dbbd0fbddeeaf2e75b92
Maxwell <nanayawfixing@gmail.com>
2023-09-23 10:58:15 +0000

    good day

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -1,437 +1,330 @@
 import Featured from "@/components/Featured/Featured";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
-  const grid = [
-    ["B", "S", "B", "S", "B", "S", "B", "S", "B", "S", "B"],
-    ["B", "S", "", "S", "B", "S", "B", "S", "B", "S",""],
-    ["B", "S", "", "S", "B", "S", "", "", "B", "", ""],
-    ["B", "S", "", "S", "B", "S", "", "", "B", "", ""],
-    ["B", "S", "", "S", "B", "S", "", "", "B", "", ""],
-    ["B", "", "", "S", "", "", "", "", "", "", ""],
-    ["B", "", "", "S", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-  ];
 
-  const maxGridLength = grid.reduce((acc, curr) => Math.max(acc, curr.length), 0);
 
-  const grid2 = [
-    ["B", "S", "B", "S", "B", "S", "B", "S", "B", "S", "B"],
-    ["B", "S", "", "S", "B", "S", "B", "S", "B", "S", ""],
-    ["B", "", "", "S", "B", "S", "", "", "B", "", ""],
-    ["B", "", "", "S", "B", "S", "", "", "B", "", "", "KK"],
-    ["B", "", "", "S", "B", "B", "B", "B", "B", "", ""],
-    ["B", "", "", "S", "S", "S", "S", "S", "B", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", "", "", ""],
-  ];
 
-  let abc: any = [
-    // [0, 0],
-  ];
-  // console.log(abc.length)
 
-  const ifExists = (i: any, j: any) => {
-    // await checkDepth(i, j)
-    if (i > 5) return [true];
-
-    // console.log(abc)
-    if (abc.length === 0) return [false];
-    return abc.map((arr: any) => arr[0] === i && arr[1] === j);
-  };
-
-  // function checkDepth(i: any) {
-  //   // console.log("iiiiiiiii",i)
-  //   if (i > 5) return [true];
-  // }
-
-  function test() {
-    for (let rows = 0; rows < 11; rows++) {
-      for (let columns = 0; columns < grid.length; columns++) {
-        // if (i == 6 && columns === 0) {
-        // if (grid[columns][rows] === "") {
-        //   // continue;
-        // }
-        console.log([rows, columns, grid[rows]?.[columns]]);
-        // console.log(ifExists(rows, columns)[0]);
-        // if (ifExists(i, columns)[0] === false) {
-        // abc.push([rows, columns, grid[rows][columns]]);
-        // }
-        // else {
-        //   console.log("else");
-        //   let remainder = i - 5;
-        //   abc.push([5, remainder, grid[i][j]]);
-        // }
-        // }
-      }
-    }
-    // console.log("abccc", abc);
-    return abc;
-  }
-
-  // B: 1, S: 2
-
-  // [row, col, value]
-  const hhh = [
-    [
-        0,
-        0,
-        "S"
-    ],
-    [
-        0,
-        1,
-        "B"
-    ],
-    [
-        0,
-        2,
-        "S"
-    ],
-    [
-        1,
-        2,
-        "S"
-    ],
-    [
-        2,
-        2,
-        "S"
-    ],
-    [
-        3,
-        2,
-        "S"
-    ],
-    [
-        0,
-        3,
-        "B"
-    ],
-    [
-        0,
-        4,
-        "S"
-    ],
-    [
-        1,
-        4,
-        "S"
-    ],
-    [
-        2,
-        4,
-        "S"
-    ],
-    [
-        3,
-        4,
-        "S"
-    ],
-    [
-        4,
-        4,
-        "S"
-    ],
-    [
-        0,
-        5,
-        "B"
-    ],
-    [
-        1,
-        5,
-        "B"
-    ],
-    [
-        0,
-        6,
-        "S"
-    ],
-    [
-        1,
-        6,
-        "S"
-    ],
-    [
-        2,
-        6,
-        "S"
-    ],
-    [
-        3,
-        6,
-        "S"
-    ],
-    [
-        4,
-        6,
-        "S"
-    ],
-    [
-        5,
-        6,
-        "S"
-    ],
-    [
-        5,
-        7,
-        "S"
-    ],
-    // [
-    //     5,
-    //     8,
-    //     "S"
-    // ],
-    // [
-    //     5,
-    //     9,
-    //     "S"
-    // ],
-    // [
-    //     0,
-    //     7,
-    //     "B"
-    // ],
-    // [
-    //     1,
-    //     7,
-    //     "B"
-    // ],
-    // [
-    //     2,
-    //     7,
-    //     "B"
-    // ],
-    // [
-    //     0,
-    //     8,
-    //     "S"
-    // ],
-    // [
-    //     1,
-    //     8,
-    //     "S"
-    // ],
-    // [
-    //     2,
-    //     8,
-    //     "S"
-    // ],
-    // [
-    //     3,
-    //     8,
-    //     "S"
-    // ],
-    [
-        4,
-        8,
-        "S"
-    ],
-    [
-        4,
-        9,
-        "S"
-    ],
-    [
-        4,
-        10,
-        "S"
-    ],
-    // [
-    //     5,
-    //     10,
-    //     "S"
-    // ],
-    // [
-    //     5,
-    //     11,
-    //     "S"
-    // ],
-    // [
-    //     0,
-    //     9,
-    //     "B"
-    // ],
-    // [
-    //     1,
-    //     9,
-    //     "B"
-    // ],
-    // [
-    //     2,
-    //     9,
-    //     "B"
-    // ],
-    // [
-    //     3,
-    //     9,
-    //     "B"
-    // ],
-    // [
-    //     0,
-    //     10,
-    //     "S"
-    // ],
-    // [
-    //     1,
-    //     10,
-    //     "S"
-    // ],
-    // [
-    //     2,
-    //     10,
-    //     "S"
-    // ],
-    // [
-    //     3,
-    //     10,
-    //     "S"
-    // ],
-    // [
-    //     3,
-    //     11,
-    //     "S"
-    // ],
-    [
-        4,
-        11,
-        "S"
-    ],
-    [
-        4,
-        12,
-        "S"
-    ],
-    [
-        5,
-        12,
-        "S"
-    ],
-    // [
-    //     5,
-    //     13,
-    //     "S"
-    // ]
-]
+  const card = ["1st", "2nd", "3rd", "4th", "5th", "sum"]
+  const [data, setData]: any = useState({
+    "1st": [],
+    "2nd": [],
+    "3rd": [],
+    "4th": [],
+    "5th": [],
+    "sum": []
+  });
 
   // console.log(grid)
-  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    let obj = {}
+    card.forEach((card) => {
+      const trees = buildTree(drawNumbers, card, "primeComposite");
+      obj[card] = trees
+    })
+    setData(obj)
+
+  }, [])
   return (
     <div>
-      {maxGridLength}
-      <br />
-      <br />
-      {JSON.stringify(data)}
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          console.log(test());
-        }}
-      >
-        test
-      </button>
-      <button onClick={() => setData(abc)}>Data</button>
-      <table>
-        <tbody>
-          {grid.map((row, i) => (
-            <tr key={i}>
-              {i}
-              {row.map((cell, j) => (
-                <td
-                  key={j}
-                  style={{
-                    border: "1px solid black",
-                    width: "15px",
-                    height: "15px",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    margin: 0,
-                    // color: values[0] === cell ? firstValueColor : "blue",
-                  }}
-                >
-                  {cell}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <br />
-      <br />
-      <table>
-        <tbody>
-          {Array(15).fill(null).map((cell, row_index) => (
-            <tr key={row_index}>
-              {row_index}
-              {Array(20).fill(null).map((cell, cell_index) => (
-                <>
-                  {/* {cell_index} */}
-                  <td
-                    key={cell_index}
-                    style={{
-                      border: "1px solid black",
-                      width: 50,
-                      // height: 5,
-                      // textAlign: "center",
-                      fontWeight: "bold",
-                      // margin: 0,
-                      // color: values[0] === cell ? firstValueColor : "blue",
-                    }}>
-                    {/* {hhh?.[0]?.[0] == row_index && hhh?.[0]?.[1] == cell_index && hhh?.[0]?.[2] == "B" ? "B" : ""} */}
-                    {hhh.map((_, index) => (
-                      <>
-                      <h1 key={index}
-                        style={{ color: hhh?.[index]?.[2] == "B" ? 'red' : 'blue' }}
-                      >
-                        {hhh?.[index]?.[0] == row_index && hhh?.[index]?.[1] == cell_index && hhh?.[index]?.[2]}
+      {/* {JSON.stringify(data)} */}
+      {card.map((card: any, index) => (
+        <div key={index}>
+          {card}
+          {/* {buildTree(drawNumbers, card)} */}
+          <table  >
+            <tbody>
+              {Array(6).fill(null).map((cell, row_index) => (
+                <tr key={row_index}>
+                  {row_index}
+                  {Array(100).fill(null).map((cell, cell_index) => (
 
-                      </h1>
-                       {/* <h4> {hhh?.[index]?.[0] == 5 && hhh?.[index]?.[1] == 6 && hhh?.[index]?.[2]}</h4> */}
-                       </>
-                    ))}
-                    {/* {row_index==5 && cell_index== 7 && "E"} */}
-                    {/* {row_index==3 && cell_index== 6 && "T"} */}
-                    {/* {row_index==4 && cell_index== 7 && "T"} */}
-                    {/* {row_index <= 5 && (
-                      <h2 style={{ color: grid[row_index]?.[cell_index] == "B" ? 'red' : 'blue' }}>{grid[row_index]?.[cell_index]} </h2>
-                    )}
-                    R{row_index}C{cell_index}
-                    {row_index == 6 && grid[row_index]?.[cell_index] === 'S' ? "" : row_index == 5 && cell_index == 4 && 
-                       <h3 style={{ color: grid[6]?.[0] == "B" ? 'red' : 'blue' }}>{grid[row_index]?.[cell_index]}S</h3>
-                    }
-                    {row_index == 6 && grid[row_index]?.[cell_index] === 'S' ? "" : row_index == 5 && cell_index == 5 && 
-                       <h3 style={{ color: grid[6]?.[0] == "B" ? 'red' : 'blue' }}>S</h3>
-                    }
-                    {row_index == 6 && grid[row_index]?.[cell_index] === 'S' ? "" : row_index == 5 && cell_index == 6 && 
-                       <h3 style={{ color: grid[6]?.[0] == "B" ? 'red' : 'blue' }}>S</h3>
-                    }
-                    {row_index == 6 && grid[row_index]?.[cell_index] === 'S' ? "" : row_index == 5 && cell_index == 7 && 
-                       <h3 style={{ color: grid[6]?.[0] == "B" ? 'red' : 'blue' }}>S</h3>
-                    } */}
-                        {/* {row_index == 5 && cell_index == 4 && "AMA"} */}
-                        {/* {grid[row_index]?.[cell_index]} */}
-                        {}
+                    <>
+                      {/* {cell_index} */}
+                      {/* {cell_index == 0 && 1} */}
+                      {/* {cell_index} */}
+                      <td
+                        key={cell_index}
+                        style={{
+                          border: "1px solid black",
+                          width: 50,
+                          fontWeight: "bold",
+                        }}>
+
+                        {data?.[card].map((_: any, index: any) => (
+                          <>
+                            <h1 key={index}
+                              style={{ color: ["B", "O", "P", "D"].includes(data?.[card]?.[index]?.[2]) ? 'red' : 'blue', textAlign: "center" }}
+                            >
+                              {data?.[card]?.[index]?.[0] == row_index && data?.[card]?.[index]?.[1] == cell_index && data?.[card]?.[index]?.[2]}
+
+                            </h1>
+
+                          </>
+                        ))}
+
                       </td>
                     </>
                   ))}
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <br /><br />
+        </div>
+      ))}
     </div>
   );
 };
 
 export default HomePage;
+
+
+
+
+
+
+
+
+const drawNumbers = [
+  [5, 2, 3, 7, 5],
+  [5, 7, 3, 7, 5],
+  [5, 2, 3, 7, 5],
+  [1, 2, 3, 4, 6],
+  [1, 2, 3, 5, 6],
+  [6, 2, 4, 6, 6],
+  [6, 2, 4, 6, 6],
+  [1, 3, 4, 5, 6],
+  [2, 3, 4, 6, 6],
+  [9, 2, 4, 6, 6],
+  [9, 3, 4, 5, 6],
+  [4, 0, 4, 5, 1],
+  [9, 7, 5, 5, 6],
+  [9, 9, 4, 8, 6],
+  [8, 3, 5, 5, 6],
+  [9, 3, 5, 5, 2],
+  [6, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 3, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 7, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 2, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+  [9, 6, 5, 5, 6],
+];
+
+
+function getBigSmallForBalls(ball: number): string {
+  return ball >= 0 && ball <= 4 ? "S" : "B";
+}
+
+function getBigSmallForBall(ball: number): string {
+  return ball >= 0 && ball <= 4 ? "S" : "B";
+}
+
+function firstAndLast<T>(arr: T[]): T[] {
+  if (arr.length === 0) {
+    return []; // Return an empty array for an empty input array.
+  }
+
+  const first = arr[0];
+  const last = arr[arr.length - 1];
+
+  return [first, last];
+}
+
+// Helper functions
+function getOddEven(num: number): string {
+  return num % 2 === 0 ? "E" : "O";
+}
+
+function getBigSmallForSum(sum: number): string {
+  return sum >= 23 && sum <= 45 ? "B" : "S";
+}
+
+function getDragonTiger(lastTwo: string[]): string {
+  if (lastTwo[0] > lastTwo[1]) {
+    return "D";
+  } else if (lastTwo[0] < lastTwo[1]) {
+    return "T";
+  } else {
+    return "A";
+  }
+}
+
+function getPrimeComposite(ball: number): string {
+  const primes = [1, 2, 3, 5, 7];
+
+  if (primes.includes(ball)) {
+    return "P";
+  } else {
+    return "C";
+  }
+}
+
+function analyzeDraw(drawNumbers: number[][], whatToAnalyze: string): {
+  smallBig: string[];
+  oddEven: string[]; // Add an array to store detailed
+  primeComposite: string[];
+} {
+  const results: any = {
+    smallBig: [], // Add an array to store detailed dragon-tiger-tie counts
+    oddEven: [], // Add an array to store detailed
+    primeComposite: [],
+  };
+
+  let prevBigSmall;
+
+  for (const element of drawNumbers) {
+    const draw: any = element;
+
+    let ball;
+
+    ball = draw[parseInt(whatToAnalyze) - 1];
+
+    // let bigSmall = getBigSmallForBall(ball);
+
+    // let ball;
+    if (whatToAnalyze === "sum") {
+      ball = draw.reduce((a: number, b: number) => a + b, 0);
+    } else {
+      ball = draw[parseInt(whatToAnalyze) - 1];
+    }
+
+    let oddEven, bigSmall, primeComposite, dragonTiger;
+
+    if (whatToAnalyze === "sum") {
+      console.log("dsfkjksdafkhsadhfkhsdakhfklsdfklhsdaf")
+      oddEven = getOddEven(ball);
+      bigSmall = getBigSmallForSum(ball);
+
+      let lastTwo = firstAndLast(draw);
+      dragonTiger = getDragonTiger(lastTwo as []);
+    } else {
+      oddEven = getOddEven(ball);
+      bigSmall = getBigSmallForBall(ball);
+
+      primeComposite = getPrimeComposite(ball);
+      dragonTiger = getDragonTiger(["dragon", "tiger", "tie"]); // Initialize dragonTiger value
+    }
+    // Add the bigSmall value to the results array
+    results.smallBig.push(bigSmall);
+    results.oddEven.push(oddEven);
+    results.primeComposite.push(primeComposite);
+
+    // Update prev values
+    prevBigSmall = bigSmall;
+  }
+
+  return results;
+}
+
+function buildTree(drawNumbers: number[][], whatToAnalyze: string, type: string ): [number, number, string][] {
+  const tree: [number, number, string][] = [];
+  const analyzedResults = analyzeDraw(drawNumbers, whatToAnalyze)[type];
+  // const analyzedResults1 = analyzeDraw(drawNumbers, whatToAnalyze).oddEven;
+  // const analyzedResults2 = analyzeDraw(drawNumbers, whatToAnalyze).primeComposite;
+  
+  console.log(analyzedResults);
+  let row = 0;
+  let col = 0;
+  let whenColIncreaseByMaxRows = 0;
+  let dua: Record<string, string> = {};
+
+  analyzedResults.forEach((value: any, i: number) => {
+    let previousLetter = analyzedResults[i - 1];
+    let currentLetter = analyzedResults[i];
+    let nextLetter = analyzedResults[i + 1];
+    let newItem: any = [];
+
+    if (currentLetter !== previousLetter) {
+      col = whenColIncreaseByMaxRows;
+    }
+    if (previousLetter !== undefined && currentLetter !== previousLetter) {
+      row = 0;
+      col++;
+      if (row === 0) {
+        whenColIncreaseByMaxRows = col;
+      }
+    } else if (currentLetter === previousLetter) {
+      row++;
+
+      let prevCol = col - 1;
+      const prev = dua[row + "|" + prevCol];
+
+      if (prev === currentLetter) {
+        row--;
+        col++;
+      }
+    } else if (
+      currentLetter !== previousLetter &&
+      currentLetter !== nextLetter
+    ) {
+      col = 0;
+      whenColIncreaseByMaxRows = col;
+    }
+
+    if (row > 5) {
+      row = 5;
+      col++;
+    }
+    console.log("row", row, "col", col, "currentLetter", currentLetter);
+    newItem = [row, col, currentLetter];
+
+    let duplicate = tree.some(
+      (item) => item[0] === newItem[0] && item[1] === newItem[1]
+    );
+
+    if (duplicate) {
+      row--;
+      col++;
+      newItem = [row, col, currentLetter];
+    }
+
+    dua[row + "|" + col] = currentLetter;
+    tree.push(newItem);
+  });
+
+  // console.log(previousValues);
+  console.log(tree);
+  return tree;
+}
